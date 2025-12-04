7c58f3ee2e7f8c79bb118fc37cb74d582588ecd8
Dmytro Usoltsev <bub3nek@Dmytros-MacBook-Pro.local>
2025-12-03 20:08:58 -0600

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
 import { Preloader } from "@/components/Preloader";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Project Proofy • by Dmytro Usoltsev",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="antialiased" suppressHydrationWarning>
         <Preloader />
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
